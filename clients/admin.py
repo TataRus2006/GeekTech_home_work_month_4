e978a07904e19fb884c1186e1ecb71005372db84
rustam <Rusik2006@gmail.com>
2022-08-26 13:56:55 +0600

    deleted media and added hw4

--- clients/admin.py
+++ clients/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
-from .models import Client
+from .models import Client, Order, BottlesCount
 
 
 admin.site.register(Client)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    list_display = ["name", "contacts", "created_at", "finished"]
+    list_editable = ["contacts", "finished"]
+    fields = ["name", "contacts", "created_at", "updated_at", "description", "finished"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class BottlesCountAdmin(admin.ModelAdmin):
+    model = BottlesCount
+    list_display = ["order", "bottle", "count", "finished"]
+    list_editable = ["bottle", "count", "finished"]
+    fields = ["order", "bottle", "count", "finished"]
+
+
+admin.site.register(BottlesCount, BottlesCountAdmin)
--- clients/models.py
+++ clients/models.py
@@ -12,3 +12,43 @@ class Client(models.Model):
         null=True,
         blank=True
     )
+
+class Order(models.Model):
+    client = models.ForeignKey(
+        to=Client, null=True, blank=True,
+        on_delete=models.SET_NULL,
+        related_name="order"
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Дата и время создания заказа",
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        verbose_name="Дата и время изменения заказа",
+        auto_now=True,
+    )
+
+    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=255)
+    contacts = models.CharField(max_length=255)
+    finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} - {self.contacts}"
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+
+class BottlesCount(models.Model):
+    bottle = models.ForeignKey(to=Client, on_delete=models.SET_NULL, null=True, blank=True, related_name="bottle")
+    count = models.IntegerField()
+    order = models.ForeignKey(to=Order, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.bottle} {self.count} {self.order}"
